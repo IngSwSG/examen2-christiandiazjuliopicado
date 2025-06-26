11330e30f64936e4a2c3dfb361b98f8d7f290cab
CHRISTIAN.DIAZGARCIA <CHRISTIAN.DIAZGARCIA@ucr.ac.cr>
2025-06-25 18:58:25 -0600

    Christian Díaz: Test del endpoint encargado de insertar materiales

---
+++ tests/Feature/MaterialControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Categoria;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('dadoUnMaterialQueNoExiste_insertarMaterial_funcionaCorrectamente', function () {
+
+    $categoria = Categoria::create(['nombre' => 'Categoria Test']);
+    $data = [
+        'codigo' => 999,
+        'unidadMedida' => 'unidad',
+        'descripcion' => 'Material de prueba',
+        'ubicacion' => 'Bodega Test',
+        'idCategoria' => $categoria->id,
+    ];
+
+
+    $response = $this->post('/materiales', $data);
+
+
+    $response->assertStatus(201);
+    $this->assertDatabaseHas('materials', [
+        'codigo' => 999,
+        'descripcion' => 'Material de prueba',
+    ]);
+});
